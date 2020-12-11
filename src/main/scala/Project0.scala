4def03ac38d5c0091679d5e2554e910eb1406d71
Cody Piazza <codypiazza@gmail.com>
2020-12-10 19:14:27 -0500

    removed oauth token information (security risk)

--- src/main/scala/Project0.scala
+++ src/main/scala/Project0.scala
@@ -1,14 +1,30 @@
 import scalaj.http._
 import scala.sys.process._
+import scala.io.StdIn.readLine
 
 object Project0 extends App {
     var alliance = 0
     var horde = 0
 
-    // val cmd = Seq("curl", "-u", "0a3af65843374ce78fcbd94f4791164c:1V5TKrj3QUZsRIDE3b3PaI3UMyh27eHJ","-d", "grant_type=client_credentials","https://us.battle.net/oauth/token")
+    // val cmd = Seq("curl", "-u", ":","-d", "grant_type=client_credentials","https://us.battle.net/oauth/token")
     // val output = cmd.!!
     // println("Ermagurd heres the output! : " + output)
 
+    println("Welcome to Cody Piazza's project zero!")
+    //Thread.sleep(1000)
+    println()
+    println("This project will analyze the current World of Warcraft pvp season.")
+    //Thread.sleep(1000)
+    println()
+    while (true) {
+        println("Options:")
+        println("2 --> Analyze the current 2v2 arena bracket")
+        println("3 --> Analyze the current 3v3 arena bracket")
+        val desiredCount = readLine()
+        if (desiredCount == "2" || desiredCount == "3") println("Valid!")
+        else println("Invalid!")
+    }
+    
 
     val threes = io.Source.fromFile("threes.csv")
     for (line <- threes.getLines()) {
